b673c92c28ad766776bdc3285e9f02e4407b9186
Armx <114233771+Armx-123@users.noreply.github.com>
2024-10-13 14:14:10 +0530

    Update write.py

--- Python Files/data/write.py
+++ Python Files/data/write.py
@@ -1,9 +1,9 @@
 import requests
 import json
-
+import os
 
 def mess(file,id):
-    TOKEN = 'MTIyNzg5NzY2MjU3MzkwMzg3Mg.GVRtbK.YymzvxH7wF4t6yXlK1QFGSAr9vAIHmYSzNDDio'
+    TOKEN = os.environ['TOKEN']
     DATA_CHANNEL_ID = id
 
 

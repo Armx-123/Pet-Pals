b98de853bd673d5123c7cb038cb51d7c361081ac
Armx <114233771+Armx-123@users.noreply.github.com>
2024-10-26 21:23:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 
 
 try:
+        subprocess.run(["python", "Python Files/files.py"], check=True)
         for i in range(0,3, 1):
-                subprocess.run(["python", "Python Files/files.py"], check=True)
+                
                 subprocess.run(["python", "Python Files/data/read.py"], check=True)
                 subprocess.run(["python", "Python Files/download.py"], check=True)
                 subprocess.run(["python", "Python Files/fps.py"], check=True)
